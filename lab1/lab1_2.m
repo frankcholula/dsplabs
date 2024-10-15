bba3429e9b3cc7c47b7ce4ad81008150c94b23e6
frankcholula <lu.phrank@gmail.com>
2024-10-15 12:51:29 +0100

    refactor part2

--- lab1/lab1_2.m
+++ lab1/lab1_2.m
@@ -6,7 +6,7 @@ nx = -2:2;
 [y, ny] = convolve(x, x, nx, nx);
 subplot(3,2,1);
 stem(ny, y);
-title('Convolution: x * x');
+title('(i) Convolution: x * x');
 xlabel('n');
 ylabel('y[n]');
 
@@ -18,7 +18,7 @@ nh = 0:1;
 [y, ny] = convolve(x, h, nx, nh);
 subplot(3,2,2);
 stem(ny, y);
-title('Convolution: x * h with h = [1, 1]');
+title('(ii) Convolution: x * h with h = [1, 1]');
 xlabel('n');
 ylabel('y[n]');
 
@@ -30,7 +30,7 @@ nh = 0:1;
 [y, ny] = convolve(x, h, nx, nh);
 subplot(3,2,3);
 stem(ny, y);
-title('Convolution: x * h with h = [0.5, 0.5]');
+title('(iii) Convolution: x * h with h = [0.5, 0.5]');
 xlabel('n');
 ylabel('y[n]');
 
@@ -43,7 +43,7 @@ nh = 0:1;
 [y, ny] = convolve(xtilde, h, nx, nh);
 subplot(3,2,4);
 stem(ny, y);
-title('Convolution: xtilde * h with x = [1,2,3,4,3,2]');
+title('(iv) Convolution: xtilde * h with x = [1,2,3,4,3,2]');
 xlabel('n');
 ylabel('y[n]');
 
@@ -56,7 +56,7 @@ nh = 0:1;
 [y, ny] = convolve(xtilde, h, nx, nh);
 subplot(3,2,5); % Fifth subplot (row 3, col 1)
 stem(ny, y)
-title('Convolution: xtilde * h with x = [0,0,1,1]')
+title('(v) Convolution: xtilde * h with x = [0,0,1,1]')
 xlabel('n');
 ylabel('y[n]');
 
