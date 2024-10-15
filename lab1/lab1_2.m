bad4f1aeb5834ed1f7b9c6223dff9ff8163b55fd
frankcholula <lu.phrank@gmail.com>
2024-10-15 12:49:55 +0100

    finished part 2.

--- lab1/lab1_2.m
+++ lab1/lab1_2.m
@@ -1,11 +1,10 @@
-% Create figure for subplots
 figure;
 
 % i) y = x * x, with x = [0, 0, 1, 1, 1]
 x = [0,0,1,1,1];
 nx = -2:2;
 [y, ny] = convolve(x, x, nx, nx);
-subplot(3,1,1); % First subplot
+subplot(3,2,1);
 stem(ny, y);
 title('Convolution: x * x');
 xlabel('n');
@@ -17,21 +16,47 @@ nx = -2:2;
 h = [1,1];
 nh = 0:1;
 [y, ny] = convolve(x, h, nx, nh);
-subplot(3,1,2); % Second subplot
+subplot(3,2,2);
 stem(ny, y);
 title('Convolution: x * h with h = [1, 1]');
 xlabel('n');
 ylabel('y[n]');
 
-% iii) y = x * h, with x = [0, 0, 1, 1] and h = [0.5, 0.5] (rounded results)
+% iii) y = x * h, with x = [0, 0, 1, 1] and h = [0.5, 0.5]
 x = [0,0,1,1];
 nx = 0:3;
 h = [0.5,0.5];
 nh = 0:1;
 [y, ny] = convolve(x, h, nx, nh);
-subplot(3,1,3); % Third subplot
+subplot(3,2,3);
 stem(ny, y);
 title('Convolution: x * h with h = [0.5, 0.5]');
+xlabel('n');
+ylabel('y[n]');
+
+% iv) y = x * h, with x = [1,2,3,4,3,2] and h = [0.5, 0.5]
+x = [1,2,3,4,3,2];
+xtilde = repmat(x, 1, 10);
+nx = 0: length(xtilde) - 1;
+h = [0.5, 0.5];
+nh = 0:1;
+[y, ny] = convolve(xtilde, h, nx, nh);
+subplot(3,2,4);
+stem(ny, y);
+title('Convolution: xtilde * h with x = [1,2,3,4,3,2]');
+xlabel('n');
+ylabel('y[n]');
+
+% v) 
+x = [0,0,1,1];
+xtilde = repmat(x, 1, 10);
+nx = 0: length(xtilde) - 1;
+h = [0.5, 0.5];
+nh = 0:1;
+[y, ny] = convolve(xtilde, h, nx, nh);
+subplot(3,2,5); % Fifth subplot (row 3, col 1)
+stem(ny, y)
+title('Convolution: xtilde * h with x = [0,0,1,1]')
 xlabel('n');
 ylabel('y[n]');
 
